bd315614e4be5051612c3c13bb9f5c88497b37a9
Mark Ernest Matute <markernest.matute@gmail.com>
2021-10-21 06:36:59 +0800

    feature/session-verification - added additional backend verification

--- src/common/middlewares/is-authenticated.ts
+++ src/common/middlewares/is-authenticated.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import Container from 'typedi';
+import { AccountService } from '../../components/account/account.service';
 import { JWT_SIGNATURE } from '../configs/env.conf';
 import { serverErrorResponse, unauthorizedResponse } from '../responses/responses';
+import * as _ from 'lodash';
 
 export default async (request: Request, res: Response, next: NextFunction) => {
   try {
@@ -12,7 +15,7 @@ export default async (request: Request, res: Response, next: NextFunction) => {
       return unauthorizedResponse(res);
     }
 
-    // Validate Authorization Header  
+    // Validate Authorization Header
     const { authorization } = headers;
     const splitAuthorization = authorization.split('Bearer');
     if (splitAuthorization.length <= 1) {
@@ -24,15 +27,19 @@ export default async (request: Request, res: Response, next: NextFunction) => {
     try {
       const decoded = await jwt.verify(token, JWT_SIGNATURE);
 
-      // TODO verify backend
-
-      request.user = decoded;
+      // Backend verification
+      const accountService = Container.get(AccountService);
+      const user = await accountService.read({
+        userId: _.get(decoded, 'id', -1)
+      });
+      if (!user.data || !user.status) {
+        return unauthorizedResponse(res);
+      }
+      request.user = user.data;
     } catch (error) {
       return unauthorizedResponse(res);
     }
-
     await next();
-
   } catch (error) {
     return serverErrorResponse(res);
   }
